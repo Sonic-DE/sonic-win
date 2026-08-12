ed89a72b1fe7acd00b6d63044c2b6d8cb5cb21d9
Joseph Crowell <joseph.w.crowell@gmail.com>
2026-08-12 21:52:35 +1000

    rename kwin_x11 to sonic-win

--- src/main_x11.cpp
+++ src/main_x11.cpp
@@ -75,7 +75,7 @@ public:
         addWM(QStringLiteral("metacity"));
         addWM(QStringLiteral("openbox"));
         addWM(QStringLiteral("fvwm2"));
-        addWM(QStringLiteral("kwin_x11"));
+        addWM(QStringLiteral("sonic-win"));
 
         QVBoxLayout *mainLayout = new QVBoxLayout(this);
         mainLayout->addWidget(mainWidget);
@@ -333,7 +333,7 @@ void ApplicationX11::crashChecking()
     if (crashes >= 4) {
         // Something has gone seriously wrong
         AlternativeWMDialog dialog;
-        QString cmd = QStringLiteral("kwin_x11");
+        QString cmd = QStringLiteral("sonic-win");
         if (dialog.exec() == QDialog::Accepted) {
             cmd = dialog.selectedWM();
         } else {
