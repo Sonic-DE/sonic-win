f403b9c9cbd16b178cf833ed1b81a9ae42eace54
Joseph Crowell <joseph.w.crowell@gmail.com>
2026-09-22 11:38:23 +1000

    ensure thread safety in log output
    Signed-off-by: Joseph Crowell <joseph.w.crowell@gmail.com>

--- src/core/renderloop.cpp
+++ src/core/renderloop.cpp
@@ -143,8 +143,10 @@ void RenderLoopPrivate::notifyFrameDropped()
 
 void RenderLoopPrivate::notifyFrameCompleted(std::chrono::nanoseconds timestamp, std::optional<RenderTimeSpan> renderTime, PresentationMode mode, OutputFrame *frame)
 {
-    if (output && s_printDebugInfo && !m_debugOutput) {
-        m_debugOutput = std::fstream(qPrintable("kwin perf statistics " + output->name() + ".csv"), std::ios::out);
+    if (s_printDebugInfo && !m_debugOutput) {
+        // X11 has one shared render loop, without an associated output.
+        const QString outputName = output ? output->name() : QStringLiteral("x11");
+        m_debugOutput = std::fstream(qPrintable("kwin perf statistics " + outputName + ".csv"), std::ios::out);
         *m_debugOutput << "target pageflip timestamp,pageflip timestamp,render start,render end,safety margin,refresh duration,vrr,tearing,predicted render time\n";
     }
     if (m_debugOutput) {
